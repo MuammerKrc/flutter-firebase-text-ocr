291a4b6566bcc13ef541f48fcdf7d93ff4159d8f
muammerKrc <muammer03karaca@gmail.com>
2022-12-05 23:40:51 +0300

    added ios and android firebase message and tested

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,31 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
+import 'firebase_options.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  var settings = FirebaseMessaging.instance.requestPermission();
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print(message);
+    print("geldi");
+  });
+  print(fcmToken);
+
   runApp(const MyApp());
+}
+
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  print('Handling a background message ${message.messageId}');
 }
 
 class MyApp extends StatelessWidget {
